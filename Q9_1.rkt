f9f0b2e33c3010d440db82e82d63a09a546af0c5
FlorenceF2006 <ineedyuqitokissme@gmail.com>
2025-03-19 18:50:30 -0400

    Update Q9_1.rkt
    add compile main function

--- Q9_1.rkt
+++ Q9_1.rkt
@@ -1,7 +1,5 @@
 #lang racket
 
-#lang racket
-
 (define (op-trans op)
   (match op
     ['+ 'add]
@@ -14,7 +12,6 @@
 ;;
 ;;assume we have a global counter : stk-ptr
 (define stk-ptr 0)
-(define acc empty)
 ;;assume we have a global accumulative data storage mem
 (define mem empty)
 ;;get-sym returns the pysmbol with stk-ptr
@@ -24,7 +21,12 @@
 (define (add-inst inst s)
     (set! s (append s (list inst))))
 
-;;(eval-aexp) will return a pysymbol representing the results of evaluation.
+(define acc empty)
+
+;;what should it return?
+;;should it return a struct?
+;;or a list? I think it should return a list.
+;;we need a list of temporary data we can constantly cons.
 (define (eval-aexp exp)
   ;;eval-h returns a pysymbol
   (define (eval-h exp)
@@ -40,8 +42,65 @@
        pys]
       [x x]))
   (eval-h exp)
-)
+  )
 
+(define (compile prog)
+  (set! mem (second (prog)))
+  (set! prog (rest (rest prog)))
+  (define (compile-h prog)
+    (match (first prog)
+      [`(print ,exp)
+       (cond[(string? exp)
+             (add-inst (list 'print-string exp))]
+            [else
+             (define pys (eval-aexp exp))
+             (add-inst (list 'print-val pys))])]
+      [`(set ,id ,aexp)
+       (define pys (eval-aexp exp))
+       (add-inst (list 'move id pys) acc)]
+      [`(iif ,bexp ,stmt1 ,stmt2)
+       (define bs (eval-bexp bexp))
+       (define tl (get-sym stk-ptr))
+       (set! stk-ptr (+ stk-ptr 1))
+       (define fl (get-sym stk-ptr))
+       (set! stk-ptr (+ stk-ptr 1))
+       (add-inst (list 'branch bs tl) acc)
+       (add-inst (list 'jump fl) acc)
+       (add-inst (list 'label tl) acc)
+       (compile-h stmt1)
+       (define nl (get-sym stk-ptr))
+       (set! stk-ptr (+ stk-ptr 1))
+       (add-inst (list 'jump nl) acc)
+       (add-inst (list 'label fl) acc)
+       (compile-h stmt2)
+       (add-inst (list 'label nl) acc)]
+      [`(skip) void]
+      [_
+       (cond[(equal? (first (first prog)) 'seq)
+             (compile-h (rest (first porg)))]
+            [else
+             (define (first prog) loop)
+             (define (rest prog) bdy)
+             (set! bdy (cons 'seq bdy))
+             (define (second loop) bexp)
+             (define bs (eval-bexp bexp))
+             (define top (get-sym stk-ptr))
+             (set! stk-ptr (+ stk-ptr))
+             (define tl (get-sym stk-ptr))
+             (set! stk-ptr (+ 1 stk-ptr))
+             (define fl (get-sym stk-ptr))
+             (set! stk-ptr (+ 1 stk-ptr))
+             (add-inst (list 'label top) acc)
+             (add-inst (list 'branch bs tl) acc)
+             (add-inst (list 'jump fl) acc)
+             (add-inst (list 'label tl) acc)
+             (compile-h bdy)
+             (add-inst (list 'jump top) acc)
+             (add-inst (list 'label fl))])])))
+       
+       
+       
+  
 
 
 
@@ -71,5 +130,3 @@
              
        
        
-
-
