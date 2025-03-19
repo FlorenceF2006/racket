c72814af0d4e093c179479e09aeb738ff04b15c8
FlorenceF2006 <ineedyuqitokissme@gmail.com>
2025-03-19 18:03:19 -0400

    Update Q9_1.rkt
    add eval-aexp function

--- Q9_1.rkt
+++ Q9_1.rkt
@@ -1,2 +1,78 @@
 #lang racket
 
+#lang racket
+
+(define (op-trans op)
+  (match op
+    ['+ 'add]
+    ['* 'mul]
+    ['- 'sub]
+    ['div 'div]
+    ['mod 'mod]
+    
+    ))
+;;
+;;assume we have a global counter : stk-ptr
+(define stk-ptr 0)
+;;assume we have a global accumulative data storage mem
+(define mem empty)
+;;get-sym returns the pysmbol with stk-ptr
+(define (get-sym ptr)
+  (+ '_' (number->symbol ptr)))
+
+(define (add-inst inst s)
+    (set! s (append s (list inst))))
+
+;;what should it return?
+;;should it return a struct?
+;;or a list? I think it should return a list.
+;;we need a list of temporary data we can constantly cons.
+(define (eval-aexp exp)
+  (define acc empty)
+  ;;eval-h returns a pysymbol
+  (define (eval-h exp)
+    (match exp
+      [`(,op ,exp1 ,exp2)
+       (define op (op-trans op))
+       (define opd1 (eval-h exp1))
+       (define opd2 (eval-h exp2))
+       (define pys (get-sym ptr))
+       (add-inst (list op pys opd1 opd2) acc)
+       (add-inst (list 'data pys 0) mem)
+       (set! stk-ptr (+ stk-ptr 1))
+       pys]
+      [x x]))
+  (eval-h exp)
+  acc)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+             
+       
+       
+
+
